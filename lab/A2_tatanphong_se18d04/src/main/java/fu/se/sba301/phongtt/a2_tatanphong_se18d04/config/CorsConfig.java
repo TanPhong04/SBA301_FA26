14e25b2514aac3fce17df989e924d91ca7e8cf7a
timani2 <tanphong0730@gmail.com>
2026-02-03 10:14:08 +0700

    update

--- lab/A2_tatanphong_se18d04/src/main/java/fu/se/sba301/phongtt/a2_tatanphong_se18d04/config/CorsConfig.java
+++
@@ -1,4 +0,0 @@
-package fu.se.sba301.phongtt.a2_tatanphong_se18d04.config;
-
-public class CorsConfig {
-}
--- lab/A2_tatanphong_se18d04/src/main/java/fu/se/sba301/phongtt/a2_tatanphong_se18d04/pojos/SystemAccount.java
+++
@@ -1,4 +0,0 @@
-package fu.se.sba301.phongtt.a2_tatanphong_se18d04.pojos;
-
-public class SystemAccount {
-}
